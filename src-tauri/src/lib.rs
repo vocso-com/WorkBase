95db7298d6e6c9fcfb4118df6ac0200cf1682ab6
Deepak Chauhan <deepak@vocso.com>
2026-07-29 16:24:12 +0530

    feat: scaffold Tauri + React + TS + Vite + Tailwind + Vitest

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_dialog::init())
+        .run(tauri::generate_context!())
+        .expect("error while running Manage");
+}
